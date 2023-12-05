7a08423f8617029bb70e9b74902db9d03ff8fcc5
Tomas Zigmantavičius <tzigmantavicius@gmail.com>
2023-12-05 21:16:32 +0200

    Solve Day4

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -4,11 +4,13 @@ namespace Day4
 {
     class Program
     {
+        static StringSplitOptions splitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
         static void Main(string[] args)
         {
             var inputs = AoCUtils.ReadDaysInput(4);
 
             Console.WriteLine(Part1(inputs));
+            Console.WriteLine(Part2(inputs));
             
         }
 
@@ -20,8 +22,7 @@ namespace Day4
                 int indexOfColon = line.IndexOf(':');
                 string actualValues = line.Substring(indexOfColon + 1);
                 string[] winningNumbersVsInputs = actualValues.Split('|');
-
-                var splitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+                
                 string[] winningNumbersSplit = winningNumbersVsInputs[0].Trim().Split(' ', splitOptions);
                 string[] inputNumbersSplit = winningNumbersVsInputs[1].Trim().Split(' ', splitOptions);
 
@@ -45,6 +46,56 @@ namespace Day4
             }
 
             return sumOfPoints;
+        }
+        
+        private static int Part2(List<string> inputs)
+        {
+            int nOfCardsToAdd = 0;
+            Dictionary<int, int> cardsAndAmount = new Dictionary<int, int>();
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                if (!cardsAndAmount.ContainsKey(i))
+                {
+                    cardsAndAmount[i] = 0;
+                }
+
+                cardsAndAmount[i]++;
+                
+                int nOfWins = 0;
+                int indexOfColon = inputs[i].IndexOf(':');
+                string actualValues = inputs[i].Substring(indexOfColon + 1);
+                string[] winningNumbersVsInputs = actualValues.Split('|');
+                
+                string[] winningNumbersSplit = winningNumbersVsInputs[0].Trim().Split(' ', splitOptions);
+                string[] inputNumbersSplit = winningNumbersVsInputs[1].Trim().Split(' ', splitOptions);
+                
+                foreach (var winningNumber in winningNumbersSplit)
+                {
+                    foreach (var inputNumber in inputNumbersSplit)
+                    {
+                        if (winningNumber == inputNumber)
+                        {
+                            nOfWins++;
+                            break;
+                        }
+                    }
+                }
+
+                for (int j = 0; j < nOfWins; j++)
+                {
+                    if (!cardsAndAmount.ContainsKey(i + j + 1))
+                    {
+                        cardsAndAmount[i + j + 1] = 0;
+                    }
+
+                    cardsAndAmount[i + j + 1] += cardsAndAmount[i];
+                }
+
+                nOfCardsToAdd += cardsAndAmount[i];
+
+            }
+
+            return nOfCardsToAdd;
         }
     }
 }
