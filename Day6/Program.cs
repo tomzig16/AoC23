4bebae069d1b8f2990548cc0fbc7ca7b8220a7b3
tomaszi <tzigmantavicius@gmail.com>
2023-12-06 13:04:50 +0200

    Solve Day 6 part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -7,8 +7,8 @@ namespace Day6
     {
         struct RaceDetails(string durationString, string distanceString)
         {
-            public int duration = int.Parse(durationString);
-            public int distance = int.Parse(distanceString);
+            public long duration = long.Parse(durationString);
+            public long distance = long.Parse(distanceString);
         }
         static void Main(string[] args)
         {
@@ -19,40 +19,14 @@ namespace Day6
             
         }
 
-        private static int Part1(List<string> inputs)
+        private static long Part1(List<string> inputs)
         {
-            int resultMult = 1;
+            long resultMult = 1;
             List<RaceDetails> allRaces = GetRaceDetailsFromInput(inputs);
-            
-            // Formula for distance is distance = hold * duration
-            // duration = total - hold
-            // distance = hold * (total - hold)
-            // distance = - hold^2 + hold * total
-            // Final equation for our distance is y = -x^2 + xT
-            // y - our distance traveled
-            // x - how long we held the button (it is the same as the speed)
-            // T - total race duration
-            
-            // We aim for minimal distance which is linear function y' = N
-            // From that we can calculate the min and max distances and hold durations for each integer in
-            // y > y'
-            // -x^2 + xT > N
-            // -x^2 + xT - N > 0
-            // D = ( -T ± sqrt(T^2 - 4 * (-1) * (-N) ) / -2
 
             foreach (var singleRace in allRaces)
             {
-                var T = singleRace.duration;
-                var N = singleRace.distance;
-
-                double Dpos = (-T + Math.Sqrt((T * T) - (4 * (-1) * (-N)))) / -2; 
-                double Dneg = (-T - Math.Sqrt((T * T) - (4 * N))) / -2;
-
-                // + 1 / -1 and then flooring/ceiling ensures that we get correct result excluding the "tie" scenario for the race
-                int finalDPos = (int)MathF.Floor((float)Dpos + 1);  
-                int finalDNeg = (int)MathF.Ceiling((float)Dneg - 1);
-                int possibleWaysToWin = finalDNeg - finalDPos + 1;
-                resultMult *= possibleWaysToWin;
+                resultMult *= SolveSingleRace(singleRace);
             }
 
             return resultMult;
@@ -72,10 +46,51 @@ namespace Day6
             return details;
         }
 
-        private static int Part2(List<string> inputs)
+        private static long SolveSingleRace(RaceDetails singleRace)
         {
-            int nOfCardsToAdd = 0;
-            return nOfCardsToAdd;
+            // Formula for distance is distance = hold * duration
+            // duration = total - hold
+            // distance = hold * (total - hold)
+            // distance = - hold^2 + hold * total
+            // Final equation for our distance is y = -x^2 + xT
+            // y - our distance traveled
+            // x - how long we held the button (it is the same as the speed)
+            // T - total race duration
+            
+            // We aim for minimal distance which is linear function y' = N
+            // From that we can calculate the min and max distances and hold durations for each integer in
+            // y > y'
+            // -x^2 + xT > N
+            // -x^2 + xT - N > 0
+            // D = ( -T ± sqrt(T^2 - 4 * (-1) * (-N) ) / -2
+            
+            long T = singleRace.duration;
+            long N = singleRace.distance;
+
+            double Dpos = (-T + Math.Sqrt((T * T) - (4 * (-1) * (-N)))) / -2; 
+            double Dneg = (-T - Math.Sqrt((T * T) - (4 * N))) / -2;
+
+            // + 1 / -1 and then flooring/ceiling ensures that we get correct result excluding the "tie" scenario for the race
+            long finalDPos = (long)Math.Floor(Dpos + 1);  
+            long finalDNeg = (long)Math.Ceiling(Dneg - 1);
+            long possibleWaysToWin = finalDNeg - finalDPos + 1;
+            return possibleWaysToWin;
+        }
+        
+        private static long Part2(List<string> inputs)
+        {
+            var matchString = @"\d+";
+            var allDurations = Regex.Matches(inputs[0], matchString);
+            var allDistances = Regex.Matches(inputs[1], matchString);
+            string finalDuration = "";
+            string finalDistance = "";
+            for (int i = 0; i < allDurations.Count; i++)
+            {
+                finalDistance += allDistances[i];
+                finalDuration += allDurations[i];
+            }
+
+            return SolveSingleRace(new RaceDetails(finalDuration, finalDistance));
         }
     }
 }
